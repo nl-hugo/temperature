23650e2894cb1ed60024cede3b2ff83acf6fe860
nl-hugo <info@hugojanssen.nl>
2015-04-26 20:10:21 +0200

    Updated conversion tool

--- js/knmi.convert.js
+++ js/knmi.convert.js
@@ -6,24 +6,18 @@
  * Converts a raw KNMI data text file to proper csv format.
  */
 
-function to_csv(path, callback) {
+function to_csv(data, callback) {
     "use strict";
 
-    d3.xhr(path).get(function(error, data) {
-        if (error != undefined) {
-            callback(error);
-        } else {
-            // clean the raw data
-            var clean = data.responseText.split("\n")
-                .filter(function(s) {
-                    // remove commented rows, except for the data headers
-                    return !s.startsWith("#") || s.startsWith("# STN,");
-                })
-                .map(function(s) {
-                    // remove white spaces and remaining #
-                    return s.replace(/\s+/g,"").replace("#","");
-                }).join("\n");
-            callback(clean);
-        }
-    });
+    // clean the raw data
+    var clean = data.split("\n")
+        .filter(function(s) {
+            // remove commented rows, except for the data headers
+            return !s.startsWith("#") || s.startsWith("# STN,");
+        })
+        .map(function(s) {
+            // remove white spaces and remaining #
+            return s.replace(/\s+/g,"").replace("#","");
+        }).join("\n");
+    callback(clean);
 }
